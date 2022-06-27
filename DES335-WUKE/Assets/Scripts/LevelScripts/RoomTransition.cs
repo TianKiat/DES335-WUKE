f4636c62bbe7cd2fcd870df9191e979bc4f53cbb
ElIgnis <Intervention.408@live.com>
2022-06-27 16:20:46 +0800

    Added simple room transition

---
+++ DES335-WUKE/Assets/Scripts/LevelScripts/RoomTransition.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class RoomTransition : MonoBehaviour
+{
+    [SerializeField]
+    RoomTransition otherTransition;
+    
+    bool canTransit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canTransit = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (canTransit)
+            {
+                collision.transform.position = otherTransition.transform.position;
+                otherTransition.SetCanTransit(false);       //Prevent constant tp-ing
+            }
+        }
+    }
+
+    public void SetCanTransit(bool toggle)
+    {
+        canTransit = toggle;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!canTransit)
+        {
+            canTransit = true;
+        }
+    }
+}
