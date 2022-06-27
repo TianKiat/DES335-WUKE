893e287147b0af36c760a667bf2250ebc0b8b8d7
Tian Kiat <ngtiankiat97@gmail.com>
2022-06-27 22:21:20 +0800

    Fixed healthbar glitching and spittingworm shooting direction

--- DES335-WUKE/Assets/Scripts/AgentScripts/SpittingWormEnemy.cs
+++ DES335-WUKE/Assets/Scripts/AgentScripts/SpittingWormEnemy.cs
@@ -35,6 +35,7 @@ public class SpittingWormEnemy : BaseEnemy
         Vector3 dir = playerTransform.position - transform.position;
         if (dir.sqrMagnitude <= radiusSqr)
         {
+            weapon.transform.right = dir;
             BaseBullet bullet = weapon.FireWeapon(DamageModifier);
             if (bullet)
                 bullet.IsPlayerBullet = false;
